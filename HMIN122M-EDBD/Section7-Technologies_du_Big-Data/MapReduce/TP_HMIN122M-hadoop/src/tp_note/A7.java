6e7cfddcbd6551b0ba1bf16638e3b3f6c0412d10
Alex <canton.alex@live.fr>
2020-12-21 21:54:28 +0100

    req 7 fini

--- HMIN122M-EDBD/Section7-Technologies_du_Big-Data/MapReduce/TP_HMIN122M-hadoop/src/tp_note/A7.java
+++ HMIN122M-EDBD/Section7-Technologies_du_Big-Data/MapReduce/TP_HMIN122M-hadoop/src/tp_note/A7.java
@@ -54,6 +54,7 @@ public class A7 {
 	private static final String INPUT_PATH = "input-disneyland/";
 	private static final String OUTPUT_PATH = "output/A7-";
 	private static final String OUTPUT_PATH_B = "output/B7-";
+	private static final String OUTPUT_PATH_C = "output/C7-";
 	private static final Logger LOG = Logger.getLogger(A7.class.getName());
 	private static int compt = 0;
 
@@ -118,21 +119,22 @@ public class A7 {
 					String Act_ida = words[0];
 					String Act_gender = words[3];
 					for (String rol : partitionnement("m", 10)) {
-						LOG.info(Act_ida+" "+rol+" "+Act_gender);
+//						LOG.info(Act_ida+" "+rol+" "+Act_gender);
 						context.write(new Text(Act_ida+" "+rol), new Text("Act"+Act_gender));
 					}
 				} else if (words[0].charAt(0) == 'm') { // Role
+//					LOG.info("test: "+words[0]+","+words[1]);
 					String Rol_idr = words[0];
 					String Rol_nom = words[1];
 					for (String act : partitionnement("e", 500)) {
-						LOG.info(act+" "+Rol_idr+" "+Rol_nom);
+//						LOG.info(act+" "+Rol_idr+" "+Rol_nom);
 						context.write(new Text(act+" "+Rol_idr), new Text("Rol"+Rol_nom));
 					}
 				} else if (words[0].charAt(0) == 's') { // Cachets
 					String Cac_ida = words[1];
 					String Cac_idr = words[2];
 					String Cac_montant = words[4];
-					LOG.info(Cac_ida+" "+Cac_idr+" "+Cac_montant);
+//					LOG.info(Cac_ida+" "+Cac_idr+" "+Cac_montant);
 					context.write(new Text('e'+Cac_ida+" "+'m'+Cac_idr), new Text("Cac"+Cac_montant));
 				}
 			}
@@ -205,7 +207,7 @@ public class A7 {
 				count++;
 				sum += Double.valueOf(val);
 			}
-			LOG.info(key+";"+sum/count+";"+count);
+//			LOG.info(key+";"+sum/count+";"+count);
 			context.write(key, new Text(";"+sum/count+";"+count));
 		}
 	}
@@ -221,36 +223,39 @@ public class A7 {
 			String[] words = line.split(";");  // tableau de mots
 			String nom = words[0].split("\\s")[0];
 			String gender = words[0].split("\\s")[1];
+			String avg = words[1];
+			String nb_acteur = words[2];
 			
 			if (Arrays.equals(words, emptyWords))
 				return;
 
-//			String Rol_nom = words[3];
-//			String Act_gender = words[1];
-//			String Cac_montant = words[2];
-//			context.write(new Text(Rol_nom+" "+Act_gender), new Text(Cac_montant));
+//			LOG.info(gender+nom+" "+value);
+			context.write(new Text(gender), new Text(nom+";"+avg+";"+nb_acteur));
 		}
 	}
 	
 	// Reducer C
 	// emit(gender, nom+moyenne(montant)+nb_acteur)
+	// Le nombre maximum d'acteurs par rapport au genre
 	public static class ReduceC extends Reducer<Text, Text, Text, Text> {
 		
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-			Double sum = 0.0;
-			int count = 0;
-			ArrayList<String> values_copy = new ArrayList<String>();
+			int max = 0;
+			String nom = null;
+			double avg = 0;
+			int nb_acteur = 0;
 			
-			for (Text val : values) {	
-				values_copy.add(val.toString());
+			for (Text val : values) {
+				String[] words = val.toString().split(";");
+				nb_acteur = Integer.valueOf(words[2]);
+				
+				if (nb_acteur > max) {
+					max = nb_acteur;
+					nom = words[0];
+					avg = Double.valueOf(words[1]);
+				}
 			}
-			
-//			for (String val : values_copy) {
-//				count++;
-//				sum += Double.valueOf(val);
-//			}
-//			LOG.info(key+";"+sum/count+";"+count);
-//			context.write(key, new Text(";"+sum/count+";"+count));
+			context.write(key, new Text(nom+" "+avg+" "+max));
 		}
 	}
 	
@@ -299,5 +304,24 @@ public class A7 {
 		FileOutputFormat.setOutputPath(jobB, new Path(OUTPUT_PATH_B + instant));
 
 		jobB.waitForCompletion(true);
+		
+		// Job C
+		Job jobC = new Job(conf, "C");
+		
+		jobC.setOutputKeyClass(Text.class);
+		jobC.setOutputValueClass(Text.class);
+
+		jobC.setMapperClass(MapC.class);
+		jobC.setReducerClass(ReduceC.class);
+
+		jobC.setOutputValueClass(Text.class); 
+		
+		jobC.setInputFormatClass(TextInputFormat.class);
+		jobC.setOutputFormatClass(TextOutputFormat.class);
+
+		FileInputFormat.addInputPath(jobC, new Path(OUTPUT_PATH_B + instant));
+		FileOutputFormat.setOutputPath(jobC, new Path(OUTPUT_PATH_C + instant));
+
+		jobC.waitForCompletion(true);
 	}
 }
